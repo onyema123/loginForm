e8bfa6fc1abbb728e82a1f428041d79aba4ee83f
Eluma Onyema <Eluma.O@bullseye.local>
2023-05-23 14:19:17 +0100

    first commit

---
+++ src/components/loginForm.js
@@ -0,0 +1,13 @@
+import './Loginform.css'
+const LoginForm = () => {
+    return (
+        <div>
+            <h1>Username</h1>
+            <input type="text"/>
+
+            <h1>Password</h1>
+            <input text="text" />
+        </div>
+    )
+}
+export default LoginForm;
