639bd6ca659005fcce319ba3cccb90c9485ea56a
Eluma Onyema <Eluma.O@bullseye.local>
2023-05-26 16:38:09 +0100

    first commit

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,13 +1,56 @@
 import './Loginform.css'
-const LoginForm = () => {
-    return (
-        <div>
-            <h1>Username</h1>
-            <input type="text"/>
 
-            <h1>Password</h1>
-            <input text="text" />
-        </div>
-    )
+import { Component } from 'react'
+
+class LoginForm extends Component {
+    state = {
+        firstname: "",
+        lastname: "",
+    }
+    handleChange = (event) => {
+        this.setState({
+           firstname:event.target.value 
+        })
+    }
+    handleLastNameChange = (event) => {
+        this.setState({
+            firstname:event.target.value,
+        })
+    };
+    handleSubmit = (event) => {
+        event.preventDefault()
+    console.log({
+            firstname:this.state.firstname
+        })
+    }
+    render() {
+        return (
+            <div className='container'>
+                <form onSubmit={this.handleSubmit}>
+                    <h1>Username</h1>
+                    <input
+                    onChange={this.handleChange}
+                    type="text"
+                    value={this.state.firstname} />
+                    {/* <input type="text"/> */}
+    
+                    
+                    {/* <input value={this.state.lastname}  */}
+                    {/* type='text'  */}
+                    {/* // onChange={this.handleLa} /> */}
+                    <h1>Password</h1>
+                    <input type='text' />
+                    <div className='btn2'>
+                        <button className='display-two' type='submit'>Submit</button>
+                    </div>
+                </form>
+            </div> 
+        )
+
+
+
+         
+    
+  }
 }
 export default LoginForm;
